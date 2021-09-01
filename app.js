db9208aeb42bc85c0c2a4e8c5639377520932546
qplatt <quincyplatt@gmail.com>
2021-09-01 19:26:07 +0000

    Added functionality to the buttons

--- app.js
+++ app.js
@@ -1,86 +1,79 @@
-var userScore = 0; 
-var aiScore = 0; 
+var userScore = 0;
+var aiScore = 0;
 const userScore_span = document.getElementById("user-score");
 const aiScore_span = document.getElementById("ai-score");
-const scoreBoard_div = document.querySelector(".score-board"); 
+const scoreBoard_div = document.querySelector(".scores");
 const result_p = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 function getAiChoice() {
-    const choices = ['r', 'p', 's'];
-    const rando = Math.floor(Math.random() * 3)
-    return choices[rando]
+    const choices = ['r', 'p', 's']; 
+    const rando = Math.floor(Math.random() * 3); 
+    return choices [rando];
 }
 
 function convertToWord(letter) {
     if (letter === "r") return "Rock"; 
-    if (letter === "p") return "Paper"
-    return "Scissors"
+    if (letter === "p") return "Paper"; 
+    return "Scissors"; 
 }
 
-function win(userChoice, aiChoice) {
+function win(userChoice, aiChoice) { 
     userScore++; 
-    userScore_span.innerHTML = userScore; 
-    aiScore_span.innerHTML = aiScore; 
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallAiWord = "ai".fontsize(3).sub();
-    result_p.innerHTML = `${convertToWord(smallUserWord)}${smallUserWord} beats ${convertToWord(aiChoice)}${smallAiWord}. You win! 🤩`
+    userScore_span.innerHTML = userScore;
+    aiScore_span.innerHTML = aiScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(aiChoice)}. You Win! 🥳`;
 }
 
 function lose(userChoice, aiChoice) {
-    aiScore++; 
-    userScore_span.innerHTML = userScore; 
-    aiScore_span.innerHTML = aiScore; 
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallAiWord = "ai".fontsize(3).sub();
-    result_p.innerHTML = `${convertToWord(smallUserWord)}${smallUserWord} loses to ${convertToWord(aiChoice)}${smallAiWord}. You lose... 😭`
+    compScore++; 
+    userScore_span.innerHTML = userScore;
+    aiScore_span.innerHTML = aiScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(aiChoice)}. You Lose! 😭`;
 }
 
-function tie(userChoice, aiChoice) {
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallAiWord = "ai".fontsize(3).sub();
-    result_p.innerHTML = `${convertToWord(smallUserWord)}${smallUserWord} ties ${convertToWord(aiChoice)}${smallAiWord}. A tie... 🤭`
+function tie(userChoice, aiChoice) { 
+    userScore_span.innerHTML = userScore;
+    aiScore_span.innerHTML = aiScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} ties ${convertToWord(aiChoice)}. It's A Tie... 🤭`; 
 }
 
 
 function game(userChoice) {
-    const aiChoice = getAiChoice(); 
+    const aiChoice = getAiChoice();
     switch (userChoice + aiChoice) {
         case "rs":
         case "pr":
         case "sp":
             win(userChoice, aiChoice);
             break; 
-        case "rp":
+        case "rp": 
         case "ps":
-        case "sr":
+        case "sr": 
             lose(userChoice, aiChoice);
-            break; 
-        case "pp":
+            break;
         case "rr":
+        case "pp":
         case "ss":
             tie(userChoice, aiChoice);
-                break; 
+            break;
     }
 }
 
-
 function main() {
-
-        rock_div.addEventListener('click', function() {
-        game("r");
-    });
-
-    paper_div.addEventListener('click', function() {
+    rock_div.addEventListener('click', function () {
+        game("r")
+    }); 
+    
+    paper_div.addEventListener('click', function () {
         game("p")
-    });
-
-    scissors_div.addEventListener('click', function() {
+    }); 
+    
+    scissors_div.addEventListener('click', function () {
         game("s")
-    });
-
+    }); 
 }
 
-main();
+main(); 
